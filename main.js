9b24fe85ecdcdafe764c33afda2dd05e83d7cf52
thurr9cm <143123282+thurr9cm@users.noreply.github.com>
2024-09-27 11:56:11 -0300

    s

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ function soma (num1, num2) {
 return num1+num2;  
 }
 console.log (soma (6,4));
+function areaQuadrado (lado) {
+    return lado **2;
+}
+console.log (areaQuadrado(6));
+let media= function (media) {
+    return 1+2+3/3;
+}
+console.log (2)
