cc1eeae5acead6e3af5f42584ef44100a916f10a
thurr9cm <143123282+thurr9cm@users.noreply.github.com>
2024-09-27 12:04:16 -0300

    hhh

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function areaQuadrado (lado) {
     return lado **2;
 }
 console.log (areaQuadrado(6));
-let media= function (num1, num2, num3, num3) {
-    return num1+num2+num3/num3;
+let media= function (num1, num2, num3, num4) {
+    return (num1+num2+num3)/num4;
 }
 console.log ((1,2,3,3));
