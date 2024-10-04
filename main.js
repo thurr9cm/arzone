32ea8c9921e97d1e23cdc3da27281af789708573
thurr9cm <143123282+thurr9cm@users.noreply.github.com>
2024-10-04 11:33:09 -0300

    hfgb

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ function areaQuadrado (lado) {
 }
 console.log (areaQuadrado(6));
 let media= function (num1, num2, num3, num4) {
-    return (num1+num2+num3)/num4;
+   return (num1+num2+num3)/num4;
 }
 console.log (media(1,2,3,3));
+let num1 = parseInt (prompt('Qual n?'));
+let num2 = parseInt (prompt('Qual n?'));
+let num3 = parseInt (prompt('Qual n?'));
+let media2 = function (x,y,z){
+    return (x+y+z)/3;
+};
+document.write(media2(num1, num2, num3));
